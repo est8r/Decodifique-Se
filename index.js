4103fb9e39fad61b54c4e7c3808aee4ab47c66f6
est8r <estermessias6@gmail.com>
2022-05-04 18:44:15 -0300

    Criei o js do cifra de cesar para as letras minusculas

--- index.js
+++ index.js
@@ -1,55 +1,102 @@
-var codificar = document.querySelector("#codificar");
-var decodificar = document.querySelector("#decodificar");
-var chave = document.querySelector("#chave");
 var select = document.querySelector("#select");
-var opcoes = document.querySelector(".opcao");
-var enviar = document.querySelector("#botao");
-var metodo = document.querySelector(".metodo");
-var aparecer = document.querySelector("#aparecer");
+var method = document.querySelector(".method");
 
-//INPUT DO INCREMENTO -------------------------------------
-
-select.addEventListener("click", function () {
-  if (select.value === "2") {
-    chave.style.display = "flex";
-  } else {
-    chave.style.display = "none";
-  }
-});
-
-//SUBSTITUI BOTÃO------------------------------------------
-
-metodo.addEventListener("click", function (e) {
-  if (codificar.checked) {
-    aparecer.innerHTML = `<input class="btnCode" id="btnCoder" type="button" value="Codificar" onclick="cod()">`;
-    document.querySelector("#btnCoder").addEventListener("click", coder);
-  } else if (decodificar.checked) {
-    aparecer.innerHTML = `<input class="btnCode" id="btnDecor" type="button" value="Decodificar" onclick="decod()">`;
-    document.querySelector("#btnDecor").addEventListener("click", decoder);
-  }
-});
-
-//BASE64 CODIFICAR E DECODIFICAR ------------------
-var texto = document.querySelector("#texto");
-var resposta = document.querySelector("#resposta");
+//CODIFICAR E DECODIFICAR ------------------
 
 function cod() {
-  if (select.value == "1" && codificar.checked) {
-    valor = texto.value;
-    resposta.value = btoa(valor);
-  } else if (codificar.checked && select.value == "2") {
+  var text = document.querySelector("#text");
+  var answer = document.querySelector("#answer");
+  var code = document.querySelector("#code");
+  if (select.value == "1" && code.checked) {
+    valor = text.value;
+    answer.value = btoa(valor);
+  } else if (code.checked && select.value == "2") {
     cesarCod();
   }
 }
 
 function decod() {
-  if (select.value == "1" && decodificar.checked) {
-    valor = texto.value;
-    resposta.value = atob(valor);
-  } else if (decodificar.checked && select.value == "2") {
+  var text = document.querySelector("#text");
+  var answer = document.querySelector("#answer");
+  var decode = document.querySelector("#decode");
+  if (select.value == "1" && decode.checked) {
+    valor = text.value;
+    answer.value = atob(valor);
+  } else if (decode.checked && select.value == "2") {
     cesarDecod();
   }
 }
-//CESAR CODIFICAR E DECODIFICAR ---------------------------------
+
+//cesar
+
+function cesarCod() {
+  var text = document.querySelector("#text").value;
+  var keychain = document.querySelector("#keychain").value;
+  var answer = document.querySelector("#answer");
+  var result = "";
+
+  for (var i = 0; i < text.length; i++) {
+    var step = parseInt(keychain);
+    var convert = text[i].charCodeAt();
+    if (convert >= 97 && convert <= 122) {
+      var calculation = convert + step;
+      if (calculation > 122) {
+        calculation = 96 + calculation - 122;
+      }
+      result += String.fromCharCode(calculation);
+    } else {
+      text[i];
+    }
+  }
+  answer.value = result;
+}
+
+function cesarDecod() {
+  var text = document.querySelector("#text").value;
+  var keychain = document.querySelector("#keychain").value;
+  var answer = document.querySelector("#answer");
+  var result = "";
+
+  for (var i = 0; i < text.length; i++) {
+    var step = parseInt(keychain);
+    var convert = text[i].charCodeAt();
+    if (convert >= 97 && convert <= 122) {
+      var calculation = convert - step;
+      if (calculation < 97) {
+        calculation = 122 + calculation - 96;
+      }
+      result += String.fromCharCode(calculation);
+    } else {
+      text[i];
+    }
+  }
+  answer.value = result;
+}
+
+//INPUT DO INCREMENTO -------------------------------------
+
+select.addEventListener("click", function () {
+  var keychain = document.querySelector("#keychain");
+  if (select.value === "2") {
+    keychain.style.display = "flex";
+  } else {
+    keychain.style.display = "none";
+  }
+});
+
+//SUBSTITUI BOTÃO------------------------------------------
+
+method.addEventListener("click", function (e) {
+  var decode = document.querySelector("#decode");
+  var code = document.querySelector("#code");
+  var show = document.querySelector("#show");
+  if (code.checked) {
+    show.innerHTML = `<input class="btnCode" id="btnCoder" type="button" value="Codificar" onclick="cod();creatCopy();">`;
+    document.querySelector("#btnCoder").addEventListener("click", coder);
+  } else if (decode.checked) {
+    show.innerHTML = `<input class="btnCode" id="btnDecor" type="button" value="Decodificar" onclick="decod();creatCopy();">`;
+    document.querySelector("#btnDecor").addEventListener("click", decoder);
+  }
+});
 
 //BOTÃO COPIAR ------------------------------------------------
